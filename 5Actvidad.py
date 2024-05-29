eddcb044bb019a1a65fadace8cb6e6b92c49ad6c
benjamin martinez <benjamartinez454@gmail.com>
2024-05-29 18:47:04 -0300

    hice asta la actividad 5

---
+++ 5Actvidad.py
@@ -0,0 +1,28 @@
+def creaLista():  
+    dias = []
+
+    for _ in range(7):
+        dia = int(input("ingresa un numerito:"))
+        dias.append(num)
+    return dias
+
+variable = creaLista()
+
+print(variable)
+
+def Temeperatura(lista):
+
+    DiasFrios = 0
+
+    for temperatura in lista:
+
+      if temperatura >= -10 and temperatura <= 5:
+       
+        DiasFrios = DiasFrios + 1
+
+    return  Temperatura
+
+if DiaFrio(variable) :
+    print("ah {temp}")
+else :
+    print("no se encontro positivo ")
